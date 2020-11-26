579d81a1ae66778f2ce40bc958f5eda7a0a9203d
Ayodeji Abodunrin <aboayosam@gmail.com>
2020-11-26 05:39:41 +0100

    Added repositories count

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ import { getProfileInfo, getRepositoriesInfo } from "./userApi.js";
 import { generateRepoMarkUp } from "./ui-functions.js";
 
 const userRepoList = document.querySelector(".user-repositories-list ul");
+const repositoryCount = document.querySelector("span.total-repositories");
+
+const setRepositoriesCount = (count) => {
+  repositoryCount.setAttribute("title", count);
+  repositoryCount.textContent = count;
+};
 
 const populateRepositoryList = async () => {
   const result = await getRepositoriesInfo(20);
@@ -15,6 +21,7 @@ const populateRepositoryList = async () => {
   } = result;
 
   userRepoList.innerHTML = generateRepoMarkUp(repositories);
+  setRepositoriesCount(repositories.totalCount);
 };
 
 const init = async () => {
